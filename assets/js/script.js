29cbf100e469b1eaa08bccdc20ee577cc5309a6d
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-26 12:56:25 +0330

    implementation open menu and add recipe form with materialize library in js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // we are using materialize library to handle menu and form
+
+  // nav menu
+  const menus = document.querySelectorAll(".side-menu");
+  M.Sidenav.init(menus, { edge: "right" });
+  // add recipe form
+  const forms = document.querySelectorAll(".side-form");
+  M.Sidenav.init(forms, { edge: "left" });
+});
