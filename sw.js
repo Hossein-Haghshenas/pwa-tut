98186e67d3ec712c17dd9022c17702494cff84b5
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-03-10 15:00:40 +0330

    Pre caching assets in service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,27 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "./index.html",
+  "./assets/js/app.js",
+  "./assets/js/ui.js",
+  "./assets/css/style.css",
+  "./assets/img/dish.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js",
+];
 // install service worker
 self.addEventListener("install", (e) => {
-  console.log("service worker installed successfully");
+  // console.log("service worker installed successfully");
+  e.waitUntil(caches.open(staticCacheName).then((cache) => cache.addAll(assets)));
 });
 
 // activate service worker
 self.addEventListener("activate", (e) => {
-  console.log("service worker activated successfully");
+  // console.log("service worker activated successfully");
 });
 
 // fetch event
 self.addEventListener("fetch", (e) => {
-  console.log("fetch event", e);
+  // console.log("fetch event", e);
 });
